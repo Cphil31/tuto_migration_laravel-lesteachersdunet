cf207fe1d040874d7433d8889c51fc4c5862b1bd
Cphil31 <cecilephilippe31@gmail.com>
2018-05-13 23:03:51 +0200

    Si ce n'est pas le dernier element alors tu m'affiche hr

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -7,15 +7,20 @@
 <body>
 	<h1>Events</h1>
 	<h1>{{count($events)}} Events</h1>
-	<?php foreach ($events as $event): ?>
+	@foreach ($events as $event): 
 		<article>
 			<h1>{{$event->name}}</h1>
 			<p>{{$event->description}} </p>
 			<p>{{$event->price}} Euros </p>
 			<p>Lieu: {{$event->location}}</p>
 		</article>
+		<!-- Si ce n'est pas le dernier element alors tu m'affiche hr -->
+
+		@if(! $loop->last)
 		<hr>
+		@endif
+
 		
-	<?php endforeach ?>
+	@endforeach
 </body>
 </html>
